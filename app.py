b1936d072334a8f45512ad257e80da416da7dc45
revatinavade48-collab <revatinavade48@gmail.com>
2026-07-15 17:25:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -223,5 +223,8 @@ def delete(id):
 
 # ---------------- Run ----------------
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
